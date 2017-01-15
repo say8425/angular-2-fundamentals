ea5b3d7f2a6b72520b6616a12c31c87bd0717784
Penguin <say8425@gmail.com>
2017-01-15 16:45:34 +0900

    import app-bar container

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { App } from './app';
 import { Main } from './app/containers';
+import { AppBar } from './app/ui';
 
 @NgModule({
-    declarations: [App, Main],
+    declarations: [App, Main, AppBar],
     imports: [BrowserModule],
     bootstrap: [App]
 })
